e839d2404f73e091d6598f2a83a0efd044fa8b7b
Yahia Fouda <yahiabdelmonem@gmail.com>
2025-03-28 23:09:25 +0200

    implement auth and storage

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -9,6 +9,9 @@ import { Button } from "@/components/ui/button"
 import {Form} from "@/components/ui/form"
 import FormField from "@/components/FormField"
 import { useRouter } from "next/navigation"
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword} from "firebase/auth"
+import { auth } from "@/firebase/client"
+import { signIn, signUp } from "@/lib/actions/auth.action"
 
 const AuthFormSchema = (type: FormType) => {
     return z.object({
@@ -32,12 +35,41 @@ const AuthForm = ({type}: { type: FormType}) => {
   })
  
   
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  async function onSubmit(values: z.infer<typeof formSchema>) {
     try {
         if(type === 'sign-up') {
-            toast.success('Account created successfully. Please sign in.');
-            router.push('/sign-in');
+          const {name, email, password} = values;
+          const userCredentials = await createUserWithEmailAndPassword(auth, email, password);
+          const result = await signUp({
+            uid: userCredentials.user.uid,
+            name: name!,
+            email,
+            password,
+          })
+
+          if(!result?.success) {
+            toast.error(result?.message);
+            return;
+          }
+          
+          toast.success('Account created successfully. Please sign in.');
+          router.push('/sign-in');
         } else {
+          const { email, password} = values;
+
+          const userCredential = await signInWithEmailAndPassword(auth, email, password);
+
+          const idToken = await userCredential.user.getIdToken();
+
+          if(!idToken) {
+            toast.error('Sign in Failed')
+            return;
+          }
+
+          await signIn({
+            email, idToken
+          })
+
           toast.success('Sign in successfully.')
           router.push('/')
         }
