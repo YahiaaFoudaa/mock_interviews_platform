7ec98308381d6efb60a77774c0140debbf254802
Yahia Fouda <yahiabdelmonem@gmail.com>
2025-03-30 20:50:38 +0200

    showing generated interviews

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,8 +4,13 @@ import Link from 'next/link'
 import Image from 'next/image'
 import { dummyInterviews } from '@/constants'
 import InterviewCard from '@/components/InterviewCard'
+import { getInterviewByUserId } from '@/lib/actions/auth.action'
+import { getCurrentUser } from '@/lib/actions/auth.action'
 
-const Page = () => {
+const Page = async () => {
+    const user = await getCurrentUser();
+    const userInterviews = await getInterviewByUserId(user?.id!);
+    const hasPastInterviews = userInterviews?.length! > 0;
     return (
         <>
             <section className="card-cta">
@@ -25,13 +30,13 @@ const Page = () => {
             <section className="flex flex-col gap-6 mt-8">
                 <h2>Your Interviews</h2>
                 <div className="interviews-section">
-                    {dummyInterviews.map(interview => (
-                        <InterviewCard {...interview} key={interview.id}/>
-                    ))}
-
-                    {/* <p>
-                        You have not taken any interviews yet
-                    </p> */}
+                    {hasPastInterviews ? (
+                        userInterviews?.map((interview) => (
+                            <InterviewCard {...interview} key={interview.id}/>
+                        ))
+                    ) : (
+                        <p>You have not taken any interviews yet</p>
+                    )}
                 </div>
             </section>
             <section className="flex flex-col gap-6 mt-8">
