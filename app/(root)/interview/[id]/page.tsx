2e46cc5f35f9470ef9ffce1ad7f670bfa1e9c619
Yahia Fouda <yahiabdelmonem@gmail.com>
2025-04-04 03:24:15 +0200

    interview call

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -3,9 +3,12 @@ import { getInterviewsById } from '@/lib/actions/general.action';
 import { getRandomInterviewCover } from "@/lib/utils";
 import { redirect } from 'next/navigation';
 import DisplayTechIcons from "@/components/DisplayTechIcons";
+import Agent from "@/components/Agent";
+import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const Page = async ({ params }: RouteParams) => {
     const { id } = await params;
+    const user = await getCurrentUser();
     const interview = await getInterviewsById(id);
 
     if(!interview) redirect('/')
@@ -15,7 +18,7 @@ const Page = async ({ params }: RouteParams) => {
                 <div className='flex flex-row gap-4 items-center max-sm:flex-col'>
                     <div className='flex flex-row gap-4 items-center'>
                         <Image src={getRandomInterviewCover()} alt={"cover-image"} width={40} height={40} className='rounded-full object-cover size-[40px]' />
-                        <h3 className="capitilize">{interview.role} Interview</h3>
+                        <h3 className="capitalize">{interview.role} Interview</h3>
 
                     </div>
                     <DisplayTechIcons techStack={interview.techstack} />
@@ -23,6 +26,8 @@ const Page = async ({ params }: RouteParams) => {
                 </div>
                 <p className="bg-dark-200 px-4 py-2 rounded-lg h-fit capitalize">{interview.type}</p>
             </div>
+
+            <Agent userName={user?.name} type={user?.id} interviewId={id} type="interview" questions={interview.questions}/>
         </>
     )
 }
